e90a1c709bfc2af839af90f41a9d8e58ec49046b
Paulo César Botelho Sincos <paulosincos@hotmail.com>
2025-11-06 17:33:01 -0300

    feat: improve jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  testMatch: [
+    "<rootDir>/tests/**/*.test.{ts,tsx}"
+  ],
   transform: {
     "^.+.tsx?$": ["ts-jest",{
       tsconfig: 'tsconfig.test.json'
     }],
   },
   collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/tests/"
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
